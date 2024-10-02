8575e438cdd3fd87984b54882fdb1139b67a397a
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-10-02 16:08:13 +0500

    Task9

---
+++ Tyuiu.MorozovSM.Sprint2.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.MorozovSM.Sprint2.Task0.V11.Lib;
+
+namespace Tyuiu.MorozovSM.Sprint2.Task0.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 8105;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, false, true, false, true, false };
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MorozovSM.Sprint2.Task0.V11/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MorozovSM.Sprint2.Task0.V11.Lib;
+
+namespace Tyuiu.MorozovSM.Sprint2.Task0.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 8105;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.Title = "Спринт #2 | Выполнил: Морозов С. М. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Морозов Сергей Максимович | АСОиУб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, False, True, False, True, False), при x = 8105, y = 275          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            foreach (var i in res)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
